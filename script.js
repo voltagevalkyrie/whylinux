51ebd0036b598b9246002ec551f14af643edbe9d
valkyrie <149293731+voltagevalkyrie@users.noreply.github.com>
2024-01-23 15:15:23 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// // script.js
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Your JavaScript code goes here
+// });
+// // If you have any JavaScript functionality, you can include it here
+
+// // Example: Change background color on click
+// document.getElementById('card1').addEventListener('click', function() {
+//     this.style.backgroundColor = '#ffc107'; // Change to your desired color
+// });
+
+// document.getElementById('card2').addEventListener('click', function() {
+//     this.style.backgroundColor = '#28a745'; // Change to your desired color
+// });
+
+// document.getElementById('card3').addEventListener('click', function() {
+//     this.style.backgroundColor = '#007bff'; // Change to your desired color
+// });
